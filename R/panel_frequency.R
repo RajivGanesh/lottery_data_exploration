ba62c87814c8cd6c2af9a44f9c1134deba2812b9
RajivGanesh <rajiv.ganesh94@gmail.com>
2022-03-17 02:39:12 -0400

    Function documented.

--- R/panel_frequency.R
+++ R/panel_frequency.R
@@ -1,9 +1,15 @@
+# this function returns the most repeated draw number for a user defined panel
 panel_frequency <- function (panel) {
+  # convert panel to a table
   formatted_panel <- table(panel)
+  # get the maximum value from the table (panel) for the most frequent number
   max_count <- max(formatted_panel)
+  # get the most repeated draw number using the maximum value from the table
   max_panel_value <- names(formatted_panel)[which(formatted_panel == max_count, arr.ind = TRUE)]
+  # create output vector as a data frame that contains the maximum value of the repeated number
   output_vector <- data.frame(max_count)
+  # change the name of the data frame's column to the most repeated draw number
   names(output_vector) <- c(max_panel_value)
-  
+  # return output vector to main
   return(output_vector)
 }
